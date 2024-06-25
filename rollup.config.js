a343785aa01c7671f891cacc491040e238c4acc5
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2024-06-25 15:22:43 +0300

    refactor(general): add esm and cjs exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,60 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
 
-const plugins = [
-  resolve(),
-];
-
-module.exports = [
+export default [
+  {
+    input: 'js/main.js',
+    output: [
+      {
+        file: 'dist/main.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/main.esm.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
+    ],
+  },
   {
     input: 'js/Datepicker.js',
-    output: {
-      file: 'dist/js/datepicker.js',
-      format: 'iife',
-      name: 'Datepicker',
-    },
-    plugins,
+    output: [
+      {
+        file: 'dist/Datepicker.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/Datepicker.esm.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
+    ],
   },
   {
-    input: 'js/datepicker-full.js',
-    output: {
-      file: 'dist/js/datepicker-full.js',
-      format: 'iife',
-    },
-    plugins,
-  },
-  {
-    input: 'js/lib/date.js',
-    output: {
-      file: 'test/_utils/date.js',
-      name: 'dateUtils',
-      format: 'iife'
-    },
-    plugins,
-  },
-  {
-    input: 'js/lib/dom.js',
-    output: {
-      file: 'test/_utils/dom.js',
-      name: 'domUtils',
-      format: 'iife'
-    },
-    plugins,
-  },
+    input: 'js/DateRangePicker.js',
+    output: [
+      {
+        file: 'dist/DateRangePicker.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/DateRangePicker.esm.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
+    ],
+  }
 ];
